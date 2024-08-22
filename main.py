ea1acf4f57b4a2ece8bca4ca9424010c1b26a58c
Jeremy Packard <wolfpack.adventurer@icloud.com>
2024-08-22 12:53:37 -0400

    change month working

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from models import Budget, Expense, BudgetModel, ExpenseModel
 from datetime import datetime
 
 
+current_date = datetime.now()
+current_budget_month = current_date.strftime("%-m/%Y")
+months = [f"{i}/{current_date.year}" for i in range(1, 13)]
+budget_model = BudgetModel()
+expense_model = ExpenseModel()
+
+def context(budget_month):
+    current_budget = budget_model.select_budget(budget_month)
+    budget_summary = current_budget.calc_budget()
+    budget_summary["months"] = months
+    budget_summary["current"] = budget_month
+    return budget_summary
+
+
+
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
@@ -13,18 +28,23 @@ def home():
     budget month. It then retrieves and displays the budget summary and the
     expenses for the budget.
     """
-    current_date = datetime.now()
-    current_budget_month = current_date.strftime("%-m/%Y")
-    budget_model = BudgetModel()
-    expense_model = ExpenseModel()
+    # current_date = datetime.now()
+    # current_budget_month = current_date.strftime("%-m/%Y")
+    # months = [f"{i}/{current_date.year}" for i in range(1, 13)]
+    # budget_model = BudgetModel()
+    # expense_model = ExpenseModel()
 
     if request.method == "GET":
-        current_budget = budget_model.select_budget(current_budget_month)
-        budget_summary = current_budget.calc_budget()
-        return render_template("index.html", context=budget_summary)
+        # current_budget = budget_model.select_budget(current_budget_month)
+        # budget_summary = current_budget.calc_budget()
+        # budget_summary["months"] = months
+        return render_template("index.html", context=context(current_budget_month))
+    
+    elif request.method == "POST":
+        new_month = request.form.get("chosen_month")
+        return render_template("index.html", context=context(new_month))
 
 
-### continue finishing docs on models and on main
 ##### make table bodys scrollable
 #### change budget month as the post to home
 
